f8ac5f7e16a60637373c852d5f22b9b4bf881a92
Mike Pham <mpham@nativecode.com>
2016-05-28 14:40:57 -0400

    Bootstrapped build process.

--- plugins/just-tips/gulpfile.js
+++ plugins/just-tips/gulpfile.js
@@ -1,5 +1,4 @@
-/// <reference path="typings/index.d.ts" />
-'use strict'
+'use strict';
 
 var bower = require('gulp-bower'),
   config = JSON.parse(require('fs').readFileSync('config.json')),
@@ -23,44 +22,47 @@ gulp.task('reload', () => {
     }
     process = require('child_process')
       .spawn('gulp.cmd', ['build'], { stdio: 'inherit' })
-      .on('error', (error) => utils.log(error))
   }
 })
 
 gulp.task('typings', () => {
-  return gulp.src(config.ts.typings.globs)
+  return gulp.src(config.ts.typings.target)
     .pipe(use.typings())
 })
 
-gulp.task('html', ['bower', 'less'], () => {
+gulp.task('html', ['less', 'ts'], () => {
   return gulp.src(config.html.globs)
+    .pipe(use.plumber())
     .pipe(use.cached('html'))
     .pipe(use.wiredep(config.wiredep.options))
     .pipe(gulp.dest(config.dest))
 })
 
-gulp.task('less', ['bower'], () => {
+gulp.task('less', () => {
   return gulp.src(config.less.globs)
+    .pipe(use.plumber())
     .pipe(use.cached('less'))
     .pipe(use.less())
     .on('error', use.lessReporter)
-    .pipe(use.postcss([require('autoprefixer')(), use.cleanCss]))
+    .pipe(use.postcss([require('autoprefixer')(config.autoprefixer.options), use.cleanCss]))
     .pipe(use.concat(config.less.target))
     .pipe(use.csslint())
+    .pipe(use.csslint.reporter(reporters('gulp-csslint')))
     .pipe(gulp.dest(config.dest))
 })
 
 gulp.task('php', () => {
   return gulp.src(config.php.globs)
+    .pipe(use.plumber())
     .pipe(use.cached('php'))
     .pipe(gulp.dest(config.dest))
 })
 
-gulp.task('ts', ['bower', 'typings'], () => {
+gulp.task('ts', ['ts:lint'], () => {
   return gulp.src(config.ts.target)
+    .pipe(use.plumber())
     .pipe(use.cached('ts'))
     .pipe(use.sourcemaps.init())
-    .pipe(use.tslint())
     .pipe(use.typescript(config.ts.options))
     .pipe(use.uglify())
     .pipe(use.concat(config.js.target))
@@ -68,14 +70,29 @@ gulp.task('ts', ['bower', 'typings'], () => {
     .pipe(gulp.dest(config.dest))
 })
 
+gulp.task('ts:lint', () => {
+  return gulp.src(config.ts.globs)
+    .pipe(use.plumber())
+    .pipe(use.tslint(config.ts.lint))
+    .pipe(use.tslint.report(reporters('gulp-tslint')))
+})
+
+gulp.task('package', ['bower', 'typings', 'build'], () => {
+  var npm = JSON.parse(require('fs').readFileSync('package.json'))
+  return gulp.src(config.zip.globs)
+    .pipe(use.plumber())
+    .pipe(use.zip(npm.name + '.' + npm.version + '.zip'))
+    .pipe(gulp.dest(config.dest))
+})
+
 gulp.task('default', ['build'])
 gulp.task('build', ['wiredep'])
 gulp.task('watch', ['build'], () => {
-  gulp.watch(config.bower.target, ['wiredep'])
+  gulp.watch(config.bower.target, ['build'])
   gulp.watch(config.html.globs, ['html'])
   gulp.watch(config.less.globs, ['less'])
   gulp.watch(config.php.globs, ['php'])
   gulp.watch(config.ts.globs, ['ts'])
   gulp.watch(config.gulp.globs, ['reload'])
 })
-gulp.task('wiredep', ['html', 'less', 'ts'])
+gulp.task('wiredep', ['html', 'php'])
---
+++ plugins/just-tips/src/plugin.php
@@ -0,0 +1,20 @@
+/**
+* @link              https://github/nativecode-dev/wordpress
+* @since             0.0.0
+* @package           nativecode-just-tips
+*
+* @wordpress-plugin
+* Plugin Name:       {{plugin.name}}
+* Plugin URI:        https://github/nativecode-dev/wordpress
+* Description:       {{plugin.description}}
+* Version:           {{plugin.version}}
+* Author:            support@nativecode.com
+* Author URI:        https://www.nativecode.com/wordpress
+* License:           {{pluginlicense}}
+* License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+* Text Domain:       {{plugin.name}}
+*/
+
+if ( ! defined( 'WPINC' ) or ! defined( 'ABSPATH' ) ) {
+    die( 'Nothing to do' );
+}
